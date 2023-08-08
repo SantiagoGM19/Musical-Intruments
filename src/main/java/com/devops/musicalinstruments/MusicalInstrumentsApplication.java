0cd9cdd5ff4914e41439db794e7ff60d83e2c690
Santiago Gomez <santigm1905@gmail.com>
2023-08-07 21:20:41 -0500

    first commit

---
+++ src/main/java/com/devops/musicalinstruments/MusicalInstrumentsApplication.java
@@ -0,0 +1,13 @@
+package com.devops.musicalinstruments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicalInstrumentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicalInstrumentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/musicalinstruments/MusicalInstrumentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.musicalinstruments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicalInstrumentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
