2b3995adbdea7132cea3fde12cd724f661ce7d40
Santiago Gomez <santigm1905@gmail.com>
2023-08-09 00:59:38 -0500

    environments configurations, spring profile production created

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM openjdk:17-jdk-alpine
+COPY build/libs/musical-instruments-0.0.1-SNAPSHOT.jar musical-instruments.jar
+ENTRYPOINT ["java", "-Dspring.profiles.active=production", "-jar", "musical-instruments.jar"]
---
+++ src/main/java/com/devops/musicalinstruments/config/ProductionPropertiesConfig.java
@@ -0,0 +1,11 @@
+package com.devops.musicalinstruments.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("production.properties")
+@Profile("production")
+public class ProductionPropertiesConfig {
+}
