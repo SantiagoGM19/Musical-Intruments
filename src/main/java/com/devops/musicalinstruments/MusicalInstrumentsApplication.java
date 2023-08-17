37adb2b53061ae9caff7107ac6f39de547728d4c
Santiago Gomez <santigm1905@gmail.com>
2023-08-17 18:13:57 -0500

    EnableJpaRepositories actived

--- src/main/java/com/devops/musicalinstruments/MusicalInstrumentsApplication.java
+++ src/main/java/com/devops/musicalinstruments/MusicalInstrumentsApplication.java
@@ -2,8 +2,10 @@ package com.devops.musicalinstruments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.devops.musicalinstruments.repositories")
 public class MusicalInstrumentsApplication{
 
     public static void main(String[] args) {
